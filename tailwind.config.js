c786d3a0b411e093fd9ed0b27f245110b7b5f953
SempaiEcchi <minecraftm665@gmail.com>
2024-05-15 17:36:20 +0200

    fix imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
+  content: ["*"],
   theme: {
     extend: {}
   },
